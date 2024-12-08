93ef67b134d4aab242d25d1b0a484aa94c8b66ae
Jerome Reyes <jerome.reyes64@gmail.com>
2024-12-08 23:48:16 +1300

    update changes

--- features/login.feature
+++ features/login.feature
@@ -1,6 +1,19 @@
 Feature: Login Guru99 Demo
+
+    @smoke
     Scenario: Logo presence on Guru99 Demo
       Given user launch chrome browser
       When user navigated to Guru99 Demo Login
-      When user login using username and password
+      When user login using "username" and "password"
       Then user was able to navigate to Homepage successfully
+
+      @regression
+      Scenario Outline:
+        Given user launch chrome browser
+        When user navigated to Guru99 Demo Login
+        When user login using "username" and "password"
+        Then user was able to navigate to Homepage successfully
+        Examples:
+          | username       | password |
+          | test@gmail.com | 123456 |
+          | abdsqwe        | abdsqwe |
--- steps/loginSteps.py
+++ steps/loginSteps.py
@@ -15,17 +15,23 @@ def step_impl(context):
     #raise NotImplementedError(u'STEP: When user navigated to Guru99 Demo Login')
 
 
-@when('user login using username and password')
-def step_impl(context):
-    context.driver.find_element(By.XPATH, "//input[@name='userName']").send_keys("test@gmail.com")
-    context.driver.find_element(By.XPATH, "//input[@name='password']").send_keys("123456")
+@when('user login using "{username}" and "{password}"')
+def step_impl(context, username, password):
+    context.driver.find_element(By.XPATH, "//input[@name='userName']").send_keys(username)
+    context.driver.find_element(By.XPATH, "//input[@name='password']").send_keys(password)
     context.driver.find_element(By.XPATH, "//input[@name='submit']").click()
     #raise NotImplementedError(u'STEP: When user login using username and password')
 
 
 @then('user was able to navigate to Homepage successfully')
 def step_impl(context):
-    element = context.driver.find_element(By.XPATH, "//*[contains(text(), 'Login Successfully')]")  # Replace "some-id" with the actual ID of the element
-    assert element.text == "Login Successfully", f"Assertion failed! Found text: {element.text}"
-    print("Assertion passed: The element contains 'Login Successfully'.")
+    try:
+        element = context.driver.find_element(By.XPATH, "//*[contains(text(), 'Login Successfully')]").text  # Replace "some-id" with the actual ID of the element
+    except Exception as e:
+        context.driver.close()
+        assert False, f"element not existing: {e}"
+
+    if element == "Login Successfully":
+        context.driver.close()
+        assert True, "Assertion passed: The element contains 'Login Successfully'."
     #raise NotImplementedError(u'STEP: Then user was able to navigate to Homepage successfully')
